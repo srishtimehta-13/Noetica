4a7ee00e6608e48a31b58b5d98376e196241db09
srishtimehta-13 <229166570+srishtimehta-13@users.noreply.github.com>
2026-04-20 23:51:16 +0530

    Delete padhai directory

--- padhai/python/hello.py
+++
@@ -1,5 +0,0 @@
-print("Hello World")
-
-def chai(n):
-    print(n)
-chai("lemon tea")
--- padhai/web dev/youtube/js/index.js
+++
@@ -1,2 +0,0 @@
-document.getElementById('myH1').textContent = `Hello`;
-document.getElementById('myP').textContent = 'I like Pizza!'
